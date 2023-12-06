e1b66653ad006c54843125796f053eb41a92578c
amarildo <amarildo.rajta@gmail.com>
2023-12-06 16:29:15 +0100

    refactor and optimize code use the Java Persistence API (JPA) instead of JDBC

--- apputveckling/src/main/java/admin/calendar/AppointmentAPI.java
+++ apputveckling/src/main/java/admin/calendar/AppointmentAPI.java
@@ -1,15 +1,22 @@
 package admin.calendar;
 
 import jakarta.json.bind.annotation.JsonbTypeSerializer;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.time.LocalDateTime;
 
 /**
  * This class represents an appointment from the database with only the parameters required from the
  * williamtroup/Calendar.js calendar component. Parameters are also renamed to match the calendar
  * parameters.
  */
+@Entity
+@Table(name = "APPOINTMENT")
 public class AppointmentAPI {
     public AppointmentAPI() {
     }
@@ -30,19 +37,19 @@ public class AppointmentAPI {
         this.title = title;
     }
 
-    public String getFrom() {
+    public LocalDateTime getFrom() {
         return from;
     }
 
-    public void setFrom(String from) {
+    public void setFrom(LocalDateTime from) {
         this.from = from;
     }
 
-    public String getTo() {
+    public LocalDateTime getTo() {
         return to;
     }
 
-    public void setTo(String to) {
+    public void setTo(LocalDateTime to) {
         this.to = to;
     }
 
@@ -53,21 +60,56 @@ public class AppointmentAPI {
     public void setColor(String color) {
         this.color = color;
     }
-
+/*
     public String getDuration() {
         return duration;
     }
 
     public void setDuration(String duration) {
         this.duration = duration;
+    }*/
+
+    public int getClientId() {
+        return clientId;
     }
 
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
 
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Id
+    @Column(name = "APPOINTMENT_ID")
     private int id;
+    @Column(name = "APPOINTMENT_TYPE")
     private String title;
-    private String from;
-    private String to;
-
-    private String duration;
+    @Column(name = "APPOINTMENT_FROM")
+    private LocalDateTime from;
+    @Column(name = "APPOINTMENT_TO")
+    private LocalDateTime to;
+    @Column(name = "CLIENT_ID")
+    private int clientId;
+    @Column(name = "PRODUCT_ID")
+    private int productId;
+    @Column(name = "APPOINTMENT_COLOR")
     private String color;
+    @Column(name = "APPOINTMENT_DESC")
+    private String description;
+
+    //private String duration;
 }
--- apputveckling/src/main/java/admin/calendar/AppointmentResource.java
+++ apputveckling/src/main/java/admin/calendar/AppointmentResource.java
@@ -1,33 +1,25 @@
 package admin.calendar;
 
 import jakarta.annotation.PostConstruct;
-import jakarta.annotation.Resource;
-import jakarta.ws.rs.ApplicationPath;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.core.MediaType;
 
-
 import javax.sql.DataSource;
-import java.sql.*;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Path("/appointments")
 public class AppointmentResource {
-    @Resource(name = "mysql_web")
-    private DataSource dataSource;
-
-    private List<Appointment> appointments;
+    @PersistenceContext(unitName = "mysql_web")
+    private EntityManager entityManager;
     private List<AppointmentAPI> appointmentsAPI;
     private static final Logger LOGGER = Logger.getLogger(AppointmentResource.class.getName());
 
@@ -35,13 +27,6 @@ public class AppointmentResource {
     public void init() {
         loadAppointments();
     }
-    /*
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<AppointmentAPI> getAppointments() {
-        //return appointments;
-        return appointmentsAPI;
-    }*/
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -52,58 +37,8 @@ public class AppointmentResource {
     }
 
     private void loadAppointments() {
-        //appointments = new ArrayList<>();
-        appointmentsAPI = new ArrayList<>();
-
-        try (Connection connection = dataSource.getConnection()) {
-            String sql = "SELECT * FROM APPOINTMENT";
-
-            try (PreparedStatement statement = connection.prepareStatement(sql);
-                 ResultSet resultSet = statement.executeQuery()) {
-
-                while (resultSet.next()) {
-                    //appointments.add(mapResultSetToAppointment(resultSet));
-                    appointmentsAPI.add(mapResultSetToAppointmentForAPI(resultSet));
-                }
-            }
-        } catch (SQLException e) {
-            LOGGER.log(Level.SEVERE, "SQL Exception", e);
-        }
+        appointmentsAPI = entityManager.createQuery("SELECT a FROM AppointmentAPI a", AppointmentAPI.class)
+                .getResultList();
     }
 
-    private Appointment mapResultSetToAppointment(ResultSet resultSet) throws SQLException {
-        Appointment appointment = new Appointment();
-        appointment.setAppointmentId(resultSet.getInt("APPOINTMENT_ID"));
-        appointment.setClientId(resultSet.getInt("CLIENT_ID"));
-        appointment.setProductId(resultSet.getInt("PRODUCT_ID"));
-        appointment.setAppointmentType(resultSet.getString("APPOINTMENT_TYPE"));
-        appointment.setAppointmentDate(resultSet.getDate("APPOINTMENT_DATE"));
-        appointment.setAppointmentTime(resultSet.getTime("APPOINTMENT_TIME"));
-        appointment.setAppointmentDesc(resultSet.getString("APPOINTMENT_DESC"));
-        return appointment;
-    }
-
-    /**
-     * This method maps the result set to an AppointmentAPI object (Calendar.js parameter names).
-     */
-    private AppointmentAPI mapResultSetToAppointmentForAPI(ResultSet resultSet) throws SQLException {
-        AppointmentAPI appointmentAPI = new AppointmentAPI();
-        appointmentAPI.setId(resultSet.getInt("APPOINTMENT_ID"));
-        appointmentAPI.setTitle(resultSet.getString("APPOINTMENT_TYPE"));
-        appointmentAPI.setColor(resultSet.getString("APPOINTMENT_COLOR"));
-        // Get date and time from resultSet
-        Date appointmentDate = resultSet.getDate("APPOINTMENT_DATE");
-        Time appointmentTime = resultSet.getTime("APPOINTMENT_TIME");
-        Time appointmentDuration = resultSet.getTime("APPOINTMENT_DURATION");
-        // Combine date and time into a from string
-        appointmentAPI.setFrom(appointmentDate.toString() + "T" + appointmentTime.toString() + "Z");
-        // Calculate the to time by adding the duration to the start time
-        LocalDateTime startDateTime = LocalDateTime.of(appointmentDate.toLocalDate(), appointmentTime.toLocalTime());
-        LocalTime duration = appointmentDuration.toLocalTime();
-        LocalDateTime endDateTime = startDateTime.plusHours(duration.getHour()).plusMinutes(duration.getMinute());
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
-        appointmentAPI.setTo(formatter.format(endDateTime));
-        appointmentAPI.setDuration(appointmentDuration.toString());
-        return appointmentAPI;
-    }
 }
